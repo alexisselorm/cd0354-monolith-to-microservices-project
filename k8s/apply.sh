989938622ae1b514ae4374dcdc5d79792711a204
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-08-29 23:50:10 +0100

    deployments and configuration files

---
+++ k8s/apply.sh
@@ -0,0 +1,16 @@
+#ENVIRONMENT VARIABLES
+kubectl apply -f aws-secret.yaml
+kubectl apply -f env-secret.yaml
+kubectl apply -f env-configmap.yaml
+
+#DEPLOYMENTS
+kubectl apply -f udagram-api-feed-deployment.yml
+kubectl apply -f udagram-api-user-deployment.yml
+kubectl apply -f udagram-frontend-deployment.yml
+kubectl apply -f udagram-reverseproxy-deployment.yml
+#SERVICES
+kubectl apply -f udagram-api-feed-service.yml
+kubectl apply -f udagram-api-user-service.yml
+kubectl apply -f udagram-frontend-service.yml
+kubectl apply -f udagram-reverseproxy-service.yml
+
